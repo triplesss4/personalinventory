fdf41d35631ab2a55d6c7bc5cc627a1eb2141743
triplesss4 <129306665+triplesss4@users.noreply.github.com>
2025-04-18 10:51:04 -0400

    Update form.php

--- form.php
+++ form.php
@@ -8,9 +8,9 @@ $quantity = $_POST['quantity'];
 $category = $_POST['category'];
 $brand = $_POST['brand'];         // NEW
 $size = $_POST['size'];           // NEW
-$price_sold = $_POST['price_sold'];
-$shipping_price = $_POST['shipping_price'];
-$date_sold = $_POST['date_sold'];
+$price_sold = $_POST['price_sold'] !== '' ? $_POST['price_sold'] : null;
+$shipping_price = $_POST['shipping_price'] !== '' ? $_POST['shipping_price'] : null;
+$date_sold = $_POST['date_sold'] !== '' ? $_POST['date_sold'] : null;
 
 // SQL with brand and size added
 $sql = "INSERT INTO items (name, price, quantity, category, brand, size, price_sold, shipping_price, date_sold)
